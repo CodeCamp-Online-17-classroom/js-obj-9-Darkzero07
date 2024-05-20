0a1949a6de2c3359db243ccbced1704366754c7b
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-20 07:40:30 +0700

    js lab 55

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+let key = prompt("Enter key:");
+let obj ={};
+while (key !== "stop") {
+    let val = prompt("value:");
+    obj[key] =val;
+    key = prompt("key:");  // รับค่า key ถัดไป
+}
+console.log(obj);
